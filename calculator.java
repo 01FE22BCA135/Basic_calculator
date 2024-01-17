8a12009baf217efa52655b47c50ed1ca0856dcca
prateek <01fe22bca135@kletech.ac.in>
2024-01-17 16:59:34 +0530

    Code for basic Arithmatic ops

---
+++ calculator.java
@@ -0,0 +1,39 @@
+class calculator{
+    
+    float add(float a,float b)
+    {
+     return(a+b);
+    }
+
+    float sub(float a,float b)
+    {
+     return(a-b);
+    }
+
+    float multi(float a,float b)
+    {
+     return(a*b);
+    }
+
+    float div(float a,float b)
+    {
+     return(a/b);
+    }
+
+public static void main(String[] args) {
+    
+    calculator c = new calculator();
+    float a=10;
+    float b=12;
+
+    System.out.println("Addition of two numbers:"+ c.add(a,b));
+
+    System.out.println("substraction of two numbers:"+ c.sub(a,b));
+
+    System.out.println("Multiplication of two numbers:"+ c.multi(a,b));
+
+    System.out.println("Division of two numbers:"+ c.div(a,b));
+    
+}
+
+}
